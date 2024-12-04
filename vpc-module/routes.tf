bec4792ce3105af6d9f4207df231e8c5c79fd5e4
Alexandre Oliveira <alexandrebo@gmail.com>
2024-12-04 14:43:08 +0100

    add route tables and associations for subnets

---
+++ vpc-module/routes.tf
@@ -0,0 +1,30 @@
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "alex-public_route"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "alex-private_route"
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route.id
+}
